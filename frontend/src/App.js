9f95129a59cbc5a1346355be9a3aa6f7fad22591
rafaelleitao <rafaelleitaooliveira@gmail.com>
2020-03-29 02:24:23 -0300

    Deleção de pastas geradas automaticamente pelo React e dinamização de componentes do frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+import Logon from './pages/Logon';
+import './global.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Lansei a braba <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRBijxArZIwZYtm-eOs5tEFU26Qs_PxxkHUj81TDXl8q6rnvkmq"/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Logon/>
+     
   );
 }
 
